814f6c7f3c68f2cf0cec53399c27f84a30584a0f
valramos18 <edwardramosvalencia18@gmail.com>
2025-04-11 17:55:44 -0500

    v1
    adelanto

---
+++ examen.py
@@ -0,0 +1,57 @@
+# menu para los clientes
+def menu_clientes():
+  print("Menú principal")
+  print("1. registrar clientes")
+  print("2. consultar clientes")
+  print("3. actualizar clientes")
+  print("4. eliminar clientes")
+  print("0. volver al menú principal")
+  return int(input("Selecciona una opcion valida"))
+
+def registrar_clientes():
+  nombre = input("Nombre del cliente: ")
+  id = input("identificación: ")
+  telefono = input("telefono: ")
+  direccion = input("telefono: ")
+  return(nombre,id,telefono,direccion)
+
+def buscar_cliente(id,clientes):
+  for cliente in clientes # cliente es solo un nombre de una variable y clientes es el arreglo principal
+  if clientes[1] == id: # 1 es la posicion de la tupla
+    return cliente
+  return None 
+
+def actualizar_cliente(id,clientes):
+  for i 
+  
+
+
+
+
+# menu para los bienes
+
+
+
+
+
+# zona de codigo principal
+
+clientes = [] #lista
+bienes =        #
+while true:
+  print("Menú principal")
+  print("1. gestión de los clientes")
+  print("2. gestión de los bienes")
+  print("0. salir del menú")
+  opcion = int(input("Selecciona una opcion valida"))
+  match opcion:
+  case 1:
+    while true:
+    print("")
+    print("")
+    print("")
+
+
+  
+
+  
